8b4f0533d8b86e467b18910b7d37b3894124dc93
Hassan Raza <hassanraza0406@gmail.com>
2026-06-28 19:20:24 +0500

    drop unused import

--- web/app/v/[slug]/page.tsx
+++ web/app/v/[slug]/page.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { notFound } from "next/navigation";
 import { getVenueBySlug, listVenuesBySubcategory } from "@/lib/venues";
 import { parsePhotos } from "@/lib/types";
